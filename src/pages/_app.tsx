8b1554b0d8131044be1bb4d6a972924f0ec27c6b
Rafael Almeida <rafaelalmeidatk@gmail.com>
2020-10-23 22:22:03 -0300

    Add app font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/cssreset.css';
 import '../styles/base.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
